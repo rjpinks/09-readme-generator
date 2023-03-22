03e6aa1dfa47662ef39a1fce77238d0af0681e5f
Pinky <pinkstonr54@gmail.com>
2023-03-22 16:17:03 -0500

    added all of my prompts and it seems to be working despite no loop

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 // TODO: Include packages needed for this application
-const inquirer = require("inquirer")
-const js = require("js");
+  const inquirer = require("inquirer")
+//const js = require("js");
 // TODO: Create an array of questions for user input
 const questions = [
     //I'm pretty sure that I'll want to cycle through these as I asked questions in my .prompt() for loops would be better than typing each out
-    "What's your prjoect's title?",
+    "What's your project's title?",
     "Time to provide a description--What whas your motivation/what did you learn?",
     "Are there any steps for installation?",
     "Provide me with some instructions/examples for using your app.",
-    "Time to give a shotout to any collaboraters/code that you might've used!",
+    "Time to give a shoutout to any collaboraters/code that you might've used!",
     "Are you using a license?"
 ];
 
@@ -19,16 +19,45 @@ const questions = [
 
 // TODO: Create a function to initialize app
 function init() {
-    .prompt([
-        {
-          type: 'input',
-          message: questions[0],
-          name: 'title'
-        },
-      ])
-      .then((response) => {
-        console.log("response", response);
-      });
+  // I'm not sure why but this forEach doesn't work bc it doesn't wait for each prompt
+  // questions.forEach(question => {
+    //anyways this bad boy right here is making an object which we'll make our readme from. need to look up templates next!
+    inquirer.prompt([
+      {
+        type: 'input',
+        message: questions[0],
+        name: 'title'
+      },
+      {
+        type: "input",
+        message: questions[1],
+        name: "description"
+      },
+      {
+        type: "input",
+        message: questions[2],
+        name: "installation"
+      },
+      {
+        type: "input",
+        message: questions[3],
+        name: "instructions"
+      },
+      {
+        type: "input",
+        message: questions[4],
+        name: "collaboraters"
+      },
+      {
+        type: "input",
+        message: questions[5],
+        name: "license"
+      },
+    ])
+    .then((response) => {
+      console.log("response", response);
+    });
+  // })
 }
 //I think this is the function that will have the .prompt object that will ask the questions.
 //after the questions are finished then we need to call writeToFile() bc that's going gonna make the finished product.
